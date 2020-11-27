8c299f67f8c52dc651834b2ac8248b9451c73ee6
mil <michielvdnheuvel@gmail.com>
2020-11-27 16:21:41 +0100

    Add shorter format to pmmux for lazyloader stubs

--- pmmux.sh
+++ pmmux.sh
@@ -4,6 +4,10 @@ set -xeu
 
 # FUNCTIONS -------------------------------------------------------------------
 exists() { sh -c 'command -v "$1" >/dev/null 2>&1' -- "$1"; }
+save () {
+    for x do printf %s\\n "$x"|sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/"; done
+    echo " "
+}
 
 # PACKAGE MANAGERS ------------------------------------------------------------
 pm_apk() {
@@ -81,13 +85,28 @@ pm_sh() {
 }
 
 # MAIN ------------------------------------------------------------------------
-for x in "$@"; do
-    if pm_"${x%%[+!]*}" present; then
-        case "$(echo "$x" | sed -n '1s/^[^+!]*\(.\).*/\1/p')" in
-        +) pm_"${x%%[+!]*}" + ${x#*[+!]};;
-        !) pm_"${x%%[+!]*}" ! "${x#*[+!]}";;
-        esac
-        break;
+pmmux() {
+    if [ -x "$1" ]; then
+        exec="$(save "$@")"
+        eval "set -- $(cat "$1" | sed '/^#!.*pmmux/d')"
     fi
-done
+    for x in "$@"; do
+        if pm_"${x%%[+!]*}" present; then
+            case "$(echo "$x" | sed -n '1s/^[^+!]*\(.\).*/\1/p')" in
+            +) pm_"${x%%[+!]*}" + ${x#*[+!]};;
+            !) pm_"${x%%[+!]*}" ! "${x#*[+!]}";;
+            esac
+            break;
+        fi
+    done
+    eval "set -- ${exec:-}"
+    if [ -n "${1:-}" ] && [ "$(command -v "$(basename "$1")")" != "$1" ]; then
+        exec="$(basename "$1")"; shift; set -- "$exec" "$@"
+        exec "$@"
+    else
+        echo "Lazily loading package for $(basename "$1") failed." >&2
+        exit 127
+    fi
+}
 
+pmmux "$@"
